e95c5a10fcffdad9e49a2faeaec83adb5fd6f551
MoonBready <113399839+MoonBready@users.noreply.github.com>
2023-05-06 15:26:47 -0400

    Board Manager and Game Manager
    game manager wip

--- RPG-RL/Assets/Scripts/GameManager.cs
+++ RPG-RL/Assets/Scripts/GameManager.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static GameManager instance = null;
+    private BoardManager boardScript;
+    private int level = 3;
+
+
+    void Awake()
     {
-        
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
     }
 
     // Update is called once per frame
